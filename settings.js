19b75cb3be85a36f7cce6cac4171e0edc86f3c9a
ag8o7 <server64462@gmail.com>
2024-08-07 21:37:24 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['6285840351569']
+global.botname = 'Rill Bot'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Rill Official"
+global.sticker2 = "🌜"
